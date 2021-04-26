c19250dd47cc883cc48bd6050a56cc0490a272c1
angie <angheloalf95@gmail.com>
2021-04-26 12:04:25 -0400

    print obj data

--- Z64Utils/Program.cs
+++ Z64Utils/Program.cs
@@ -29,12 +29,26 @@ namespace Z64
             Application.Run(new MainForm());
 #else
             Z64Game game = new Z64Game("baserom.z64");
-            Z64File file = game.GetFileFromIndex(200);
+            Z64File file = game.GetFileFromIndex(791);
             byte[] data = file.Data;
             Z64Object obj = new Z64Object(data);
             int segmentId = 6;
-            Z64ObjectAnalyzer.FindDlists(obj, data, segmentId, null);
+            Z64ObjectAnalyzer.Config config = new Z64ObjectAnalyzer.Config();
+            Z64ObjectAnalyzer.FindDlists(obj, data, segmentId, config);
             Z64ObjectAnalyzer.AnalyzeDlists(obj, data, segmentId);
+
+            //Console.Write(obj.ToString() + "\n");
+            //Console.Write(Z64Version.FileTable[Z64VersionEnum.OotEuropeMqDbg][] + "\n");
+
+            foreach (var entry in obj.Entries)
+            {
+                Console.Write(entry.Name + "\n");
+                /*
+                var item = listView_map.Items.Add($"{new SegmentedAddress(_segment, _obj.OffsetOf(entry)).VAddr:X8}");
+                item.SubItems.Add(entry.Name);
+                item.SubItems.Add(entry.GetEntryType().ToString());
+                */
+            }
 #endif
 
         }
